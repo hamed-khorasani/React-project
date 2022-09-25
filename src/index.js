745126d0be55500fc84aec6f96b3c873e3cfce3e
hamed-khorasani <108504202+hamed-khorasani@users.noreply.github.com>
2022-09-24 18:59:59 -0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+
+
+
+
+
+// import reportWebVitals from './reportWebVitals';
+// import App1 from './components/App';
+// import Firebase, { FirebaseContext } from './components/Firebase';
+
+// const AppBase = () => (
+//   <React.StrictMode>
+//     <FirebaseContext.Provider value={new Firebase()}>
+//       <App/>
+//     </FirebaseContext.Provider>
+//   </React.StrictMode>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <AppBase/>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
+
+// reportWebVitals();
+
+
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
